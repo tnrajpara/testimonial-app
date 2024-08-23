d32c81421b5ebf25ad7722785174bbbe06fc2d3d
Tarang Rajpara <rajparatarang66@gmail.com>
2024-08-23 08:56:24 +0530

    configure environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["i.ibb.co"],
   },
+  env: {
+    NEXT_PUBLIC_AUTH0_BASE_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000",
+  },
 };
 
 export default nextConfig;
