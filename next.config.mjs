56173bb1d3add884cec02a4d32818bb6da142fc4
Tarang Rajpara <rajparatarang66@gmail.com>
2024-08-23 09:07:42 +0530

    configure .env

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,11 @@ const nextConfig = {
   images: {
     domains: ["i.ibb.co"],
   },
-  env: {
-    NEXT_PUBLIC_AUTH0_BASE_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000",
-  },
+  // env: {
+  //   NEXT_PUBLIC_AUTH0_BASE_URL: process.env.VERCEL_URL
+  //     ? `https://${process.env.VERCEL_URL}`
+  //     : "http://localhost:3000",
+  // },
 };
 
 export default nextConfig;
